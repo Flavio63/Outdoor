a05c14ce47e1cafb381924c4bd0b6f66cd75d6e7
Flavio <flavio.villa@gmail.com>
2015-01-23 12:35:00 +0100

    this is the full version of application The part of manager db is on progress and don't work properly.

---
+++ public_html/js/views/SelectProvinces.js
@@ -0,0 +1,36 @@
+window.SelectProvinces = Backbone.View.extend({
+    tagName: 'form',
+    className: 'form-inline',
+    initialize: function(){
+        this.render();
+    },
+    render: function(){
+        $(this.el).html(this.template());
+        var provinces = this.model.models;
+        var len = provinces.length;
+        $('#territorio', this.el).append('<ol id="provinces" class="list"> </ol>');
+        for (var i = 0; i < len; i++) {
+            $('#provinces', this.el).append(new ProvinceItem({model: provinces[i]}).render().el);
+        }
+    }
+});
+
+window.ProvinceItem = Backbone.View.extend({
+    tagName: 'li',
+    className: 'province',
+    template: _.template('<span><%= DescProvincia %> (<%= Sigla %>)</span>'),
+    initialize: function(){},
+    render: function(){
+        var js = this.model.toJSON();
+        $(this.el).attr('id', js.idProvincia);
+        $(this.el).attr('data-province',{
+            idArea:js.idArea, 
+            idRegione:js.idRegione, 
+            idProvincia:js.idProvincia, 
+            descProvinca:js.descProvinca,
+            Sigla:js.Sigla
+        });
+        $(this.el).html( this.template( js ) );
+        return this;
+    }
+});
